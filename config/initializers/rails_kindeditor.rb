ba300a943320770ee4b802e161c5146a3ff57f2b
Joey wang <wxluckly@gmail.com>
2015-09-13 21:39:50 +0800

    coding

--- config/initializers/rails_kindeditor.rb
+++ config/initializers/rails_kindeditor.rb
@@ -7,7 +7,7 @@ RailsKindeditor.setup do |config|
   # Allowed file types for upload.
   config.upload_image_ext = %w[gif jpg jpeg png bmp]
   config.upload_flash_ext = %w[swf flv]
-  config.upload_media_ext = %w[swf flv mp3 wav wma wmv mid avi mpg asf rm rmvb]
+  config.upload_media_ext = %w[swf flv mp3 wav wma wmv mid avi mpg asf rm rmvb mov]
   config.upload_file_ext = %w[doc docx xls xlsx ppt htm html txt zip rar gz bz2]
   
   # Porcess upload image size
