0c0b5fa32964548872d28ebb5fc0ee474d913485
Joey wang <wxluckly@gmail.com>
2015-09-16 23:20:34 +0800

    coding

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,25 @@
 class CommentsController < ApplicationController
+  before_filter :authenticate_user!
+
   def create
     @comment = current_user.comments.new(comment_params)
     respond_to do |format|
       if @comment.save
         format.html { redirect_to "/travels/#{params[:comment][:travel_id]}#review_area" }
+        format.json { render :json => @json }
+      else
+        format.html { render action: :edit }
+        format.json { render :json => @json }
+      end
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    travel = @comment.travel
+    respond_to do |format|
+      if @comment.destroy
+        format.html { redirect_to "/travels/#{travel.id}#review_area" }
         format.json { render :json => @json }
       else
         format.html { render action: :edit }
