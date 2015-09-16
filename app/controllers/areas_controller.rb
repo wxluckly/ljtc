9a97b5da98fbb76aa64f4effc15f7f11dc885968
Joey wang <wxluckly@gmail.com>
2015-09-17 00:09:46 +0800

    coding

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,5 +1,6 @@
 class AreasController < ApplicationController
   def show
-    @travels = Travel.publishing.where(area_id: params[:id]).order('id desc').paginate(page: params[:page], per_page: 6)
+    @travels = Travel.publishing.where(area_id: params[:id]).where('event_id is null').order('id desc').paginate(page: params[:page], per_page: 6)
+    @travel_events = Travel.publishing.where(area_id: params[:id]).where('event_id is not null').order('id desc').paginate(page: params[:page], per_page: 6)
   end
 end
