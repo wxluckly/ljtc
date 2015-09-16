6a3e093cec353df66b814cf8cf52b5d20b19f1ce
Joey wang <wxluckly@gmail.com>
2015-09-16 22:38:40 +0800

    coding

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -3,7 +3,7 @@ class AjaxController < ApplicationController
 
   def like_travel
     travel = Travel.find(params[:id])
-    if TravelsAction.create(action: 'like_travel', travel_id: travel.id, user_agent: request.user_agent, user_ip: get_ip, session_id: session.id)
+    if TravelsAction.create(action: 'like_travel', travel_id: travel.id, user_agent: request.user_agent, user_ip: get_ip, session_id: session.id).valid?
       travel.increment!(:like_count)
     end
     render nothing: true
@@ -11,7 +11,7 @@ class AjaxController < ApplicationController
 
   def share_weibo
     travel = Travel.find(params[:id])
-    if TravelsAction.create(action: 'share_weibo', travel_id: travel.id, user_agent: request.user_agent, user_ip: get_ip, session_id: session.id)
+    if TravelsAction.create(action: 'share_weibo', travel_id: travel.id, user_agent: request.user_agent, user_ip: get_ip, session_id: session.id).valid?
       travel.increment!(:share_count)
     end
     render nothing: true
--- app/models/travels_action.rb
+++ app/models/travels_action.rb
@@ -1,4 +1,4 @@
 class TravelsAction < ActiveRecord::Base
-
+  validates :action, uniqueness: { scope: [:user_ip, :travel_id] }
 
 end
