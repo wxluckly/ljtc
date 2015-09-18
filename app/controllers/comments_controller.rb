ab627792689c14d387cf02c32d7a9e7f533f3c12
Joey wang <wxluckly@gmail.com>
2015-09-18 13:13:18 +0800

    coding

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
 
   def create
-    @comment = current_user.comments.new(comment_params)
+    @comment = Comment.new(comment_params.merge(user_id: current_user.try(:id)))
     respond_to do |format|
       if @comment.save
         format.html { redirect_to "/travels/#{params[:comment][:travel_id]}#review_area" }
