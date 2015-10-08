2b70ae3bc389e2f8f1d8cd7323b35862f4fba484
Joey wang <wxluckly@gmail.com>
2015-10-09 00:09:11 +0800

    coding

--- app/models/travels_action.rb
+++ app/models/travels_action.rb
@@ -1,4 +1,5 @@
 class TravelsAction < ActiveRecord::Base
-  validates :action, uniqueness: { scope: [:user_ip, :travel_id, :session_id] }
+  validates :action, uniqueness: { scope: [:travel_id, :user_ip] }
+  validates :action, uniqueness: { scope: [:travel_id, :session_id] }
 
 end
