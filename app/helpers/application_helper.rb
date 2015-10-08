4d8f2c438653103327f0bf347ce9bcd42017c5a4
Joey wang <wxluckly@gmail.com>
2015-10-09 00:04:07 +0800

    coding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def liked?(travel_id)
-    return true if TravelsAction.where(action: 'like_travel', user_ip: get_ip, travel_id: travel_id).first
+    return true if TravelsAction.where(action: 'like_travel', user_ip: get_ip, travel_id: travel_id, session_id: session.id).first
     false
   end
 
   def shared?(travel_id)
-    return true if TravelsAction.where(action: 'share_travel', user_ip: get_ip, travel_id: travel_id).first
+    return true if TravelsAction.where(action: 'share_travel', user_ip: get_ip, travel_id: travel_id, session_id: session.id).first
     false
   end
 end
