f4a5d4ad8a55b9804db74f026263ef5a5e950b12
Joey wang <wxluckly@gmail.com>
2015-10-08 23:36:25 +0800

    coding

--- app/controllers/admin/travels_actions_controller.rb
+++ app/controllers/admin/travels_actions_controller.rb
@@ -1,5 +1,5 @@
 class Admin::TravelsActionsController < Admin::BaseController
   def index
-    @travels_actions = TravelsAction.where(travel_id: params[:travel_id]).order('id desc').paginate(page: params[:page])
+    @travels_actions = TravelsAction.where(travel_id: params[:travel_id]).order('id desc').paginate(page: params[:page], per_page: 200)
   end
 end
