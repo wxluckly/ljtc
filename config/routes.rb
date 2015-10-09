2fb62a162a6df3ae4eff469ab55d13b4a2418c0a
Joey wang <wxluckly@gmail.com>
2015-10-09 11:55:32 +0800

    coding

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :staffers
+  devise_for :staffers, controllers: { registrations: "staffers/registrations" }
   devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations", passwords: "users/passwords" }
   
   root 'welcome#index'
